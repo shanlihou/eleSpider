a0f10a6cdcfbf79c88253779c1b3d4d5d8f01858
shanlihou <shanlihou@gmail.com>
2021-01-16 22:31:34 +0800

    up

--- spider/reality/config.js
+++ spider/reality/config.js
@@ -1,4 +1,3 @@
-// main_url = "https://www.mangabz.com/1362bz/"
 main_url = "http://m.90mh.com/manhua/DrSTONEshijiyuan/"
 content_scripts = [
     {
@@ -6,7 +5,6 @@ content_scripts = [
         js:["reality.js"]
     },
     {
-        // matches: [/wodexianshishilianaiyouxi\/\d+/],
         matches: [/http:\/\/m\.90mh\.com\/manhua\/\w+\/\d+/,],
         js:["page.js"]
     }
